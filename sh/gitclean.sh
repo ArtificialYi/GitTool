787bf3e2a7655ee7c4ea07a4705b796d8428b44c
huangyi <496109870@qq.com>
2024-09-27 11:43:44 +0800

    feat: gitclean-增加移除本地中不在云上的tag

--- sh/gitclean.sh
+++ sh/gitclean.sh
@@ -7,3 +7,5 @@ git rebase
 git branch -vv | grep ': 丢失]' | awk '{print $1}' | xargs -n 1 git branch -D
 # 合并过的分支
 git branch --merged master | grep -v "\* master" | xargs -n 1 git branch -d
+# 不在云上的tag
+comm -23 <(git tag | sort) <(git ls-remote --tags origin | awk -F/ '{print $3}' | sort) | xargs git tag -d
