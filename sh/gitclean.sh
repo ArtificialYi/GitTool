ece717bb60ac7b37d0e8989e73bbff6797aa6f7d
ArtificialYi <hy496109870@outlook.com>
2024-09-28 03:41:19 +0800

    feat: clean tag (#7) (#8)
    * feat: gitclean-移除本地中与云不一致的tag

--- sh/gitclean.sh
+++ sh/gitclean.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 git checkout master
 git fetch --prune
@@ -7,3 +7,5 @@ git rebase
 git branch -vv | grep ': 丢失]' | awk '{print $1}' | xargs -n 1 git branch -D
 # 合并过的分支
 git branch --merged master | grep -v "\* master" | xargs -n 1 git branch -d
+# 不在云上的tag-sh不可用，bash/zsh可用
+comm -23 <(git tag | sort) <(git ls-remote --tags origin | awk -F/ '{print $3}' | sort) | xargs git tag -d
