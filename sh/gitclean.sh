51c2c019d9e13a62614ce84a2e68f956927b6ccd
ArtificialYi <hy496109870@outlook.com>
2025-05-21 14:33:16 +0800

    pref: 兼容gone (#10)
    系统语言英文兼容

--- sh/gitclean.sh
+++ sh/gitclean.sh
@@ -4,7 +4,7 @@ git checkout master
 git fetch --prune
 git rebase
 # 被云删除的分支
-git branch -vv | grep ': 丢失]' | awk '{print $1}' | xargs -r -n 1 git branch -D
+git branch -vv | grep -E ': (丢失|gone)\]' | awk '{print $1}' | xargs -r -n 1 git branch -D
 # 合并过的分支
 git branch --merged master | grep -v "\* master" | xargs -r -n 1 git branch -d
 # 不在云上的tag-sh不可用，bash/zsh可用
